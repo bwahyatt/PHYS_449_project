d35b7232df36e75030ebaf4d3c81415f6a2282c0
rbarton515 <rachel.barton515@gmail.com>
2022-11-18 19:33:00 -0500

    created larger dataset and csv with labels

--- src/dataset_generate.py
+++ src/dataset_generate.py
@@ -3,18 +3,34 @@ import numpy as np
 import cv2
 import pandas as pd
 
-galaxy_zoo = pd.read_csv('../sdss_shortened.csv')
-mini_zoo = galaxy_zoo.iloc[:10] #small dataset to test this out on. We can increase the size to whatever we want
+galaxy_zoo = pd.read_csv('../sdss_shortened.csv') #import galaxy zoo dataset
+mini_zoo = galaxy_zoo.iloc[:310] #small dataset to test this out on. We can increase the size to whatever we want
+
+#make lists to store the IDs and classifications
+id_list = []
+class_list = []
+class_simple_list = []
 
 
 for x in range(len(mini_zoo)):
     ra = mini_zoo.iloc[x]['ra'] #Right ascension
-    dec = mini_zoo.iloc[x]['dec'] #Declination
+    dec = mini_zoo.iloc[x]['dec'] #Declination 
+    class_full = mini_zoo.iloc[x]['gz2_class'] #full classification
+    class_simple = mini_zoo.iloc[x]['gz2_class'][0] #Simple classification, i.e. spiral, elliptical, or irregular. From first letter of classification
     reg = sdss.Region(ra, dec, fov=0.033)
 
-    id = reg.nearest_objects().iloc[0]['objID'] #ID of the object nearest to the region, AKA the object we want
+    id = reg.nearest_objects().iloc[0]['objID'] #ID of the object nearest to the region, AKA the object we are looking at
     ph = sdss.PhotoObj(id) ## this argument is the SDSS ObjID
     ph.download()
 
+    id_list.append(id)
+    class_list.append(class_full)
+    class_simple_list.append(class_simple)
+
+
     img_data = ph.cutout_image(scale=0.1) #returns np array of image
     cv2.imwrite('../raw_images/{}.jpg'.format(id), img_data) #write the array to a jpg file
+
+#Put IDs and classifications into a dataframe
+labels = pd.DataFrame(data={'ID': id_list, 'Simple Classification': class_simple_list, 'Full Classification': class_list})
+labels.to_csv('../ids_and_labels.csv')
