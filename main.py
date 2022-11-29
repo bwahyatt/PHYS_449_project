0e3de639b4a1da5f54b7b83c757d51fc2c9333c3
rbarton515 <rachel.barton515@gmail.com>
2022-11-29 18:31:46 -0500

    set up .json file with hyperparameters

--- main.py
+++ main.py
@@ -7,6 +7,7 @@ import numpy as np
 import pandas as pd
 import sys
 import os
+import json
 
 from src.neural_net import Net
 from src.data_compression import * ## I think we need all of them 
@@ -14,6 +15,8 @@ from src.data_compression import * ## I think we need all of them
 ## this path can be made a variable/command line argument/json file parameter/etc later
 ## (or not)
 processed_imgs_list = os.listdir("processed_images") 
+
+
 
 ## index for the cutoff of our training vs test data
 ## e.g. training data (file names) = processed_imgs[0:train_end_index], test data = processed_imgs[train_end_index:]
@@ -23,12 +26,14 @@ train_end_index = 220 ## completely arbitrary placeholder
 train_names = processed_imgs_list[0:train_end_index]
 test_names = processed_imgs_list[train_end_index:]
 
-## hyperparameters (get a json dict or something later.. )
-## again, values are arbitrary placeholders
-epochs = 10
-learn_rate = 1e-3
-hidden_nodes = 3 ## or whatever "third of the input size" the authors use
-feature_size = 8 ## or 13 or 25 or whatever we want. But, it is something we have to pick by hand
+## Import hyperparameters from .json
+with open('param/param.json') as paramfile: ## could make the path a command line argument
+    param = json.load(paramfile)
+
+epochs = param['epochs']
+learn_rate = param['learn_rate']
+hidden_nodes = param['hidden_nodes'] ## or whatever "third of the input size" the authors use
+feature_size = param['feature_size'] ## or 13 or 25 or whatever we want. But, it is something we have to pick by hand
                  ## to recreate paper, we will probably need to try all 3 author values?
     
 ## read in our labels
